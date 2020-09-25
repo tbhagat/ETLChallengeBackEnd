5b34c7d3f160cccdfca096275f22e8f9b48f7cf1
tbhagat <tbhagat94@gmail.com>
2020-09-25 13:34:36 -0400

    Updating trigger event, & lambda code rework.

--- src/index.py
+++ src/index.py
@@ -8,20 +8,27 @@ def handler(event, context):
     jh_source_url = "https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv"
 
     nyt_df = pd.read_csv(nyt_source_url)
-    nyt_df['date'] = pd.to_datetime(nyt_df['date']).dt.strftime('%m/%d/%Y')
     nyt_df.rename(columns={"date": "Date", "cases": "Cases", "deaths": "Deaths"}, inplace=True)
     nyt_df_final= nyt_df.drop(0).reset_index(drop=True)
 
     jh_df = pd.read_csv(jh_source_url)
-    jh_df['Date'] = pd.to_datetime(jh_df['Date']).dt.strftime('%m/%d/%Y')
     jh_df_us = jh_df.loc[jh_df['Country/Region'] == "US"]
     jh_df_final = jh_df_us[["Recovered"]].astype(int).reset_index(drop=True)
 
     data = nyt_df_final.join(jh_df_final)
-    final_data = data.to_json()
-    return final_data
+    data["Date"] = pd.to_datetime(data['Date'])
+    data["Date"] = data['Date'].dt.date
 
+
+    final_data = data.to_dict()
 
     client = boto3.resource('dynamodb')
-    table = client.Table("ETLDynamoDB")
-    table.put_item(Item= {'Date': '34','company':  'microsoft'})
+    table = client.Table("Covid")
+
+    for i in final_data["Date"]:
+        table.put_item(Item= {'Date': final_data["Date"][i].isoformat(),
+                                "Cases": final_data["Cases"][i],
+                                "Deaths": final_data["Deaths"][i],
+                                "Recovered": final_data["Recovered"][i]
+
+        })
