d7daba055b5a0b2075061f0d28aa2f3b553073af
tbhagat <tbhagat94@gmail.com>
2020-09-27 22:45:03 -0400

    Adding SNS to lambda & SNS to template.yml

--- src/index.py
+++ src/index.py
@@ -22,15 +22,16 @@ def handler(event, context):
 
     client = boto3.resource('dynamodb')
     table = client.Table(os.environ['TABLE_NAME'])
-    current_db_data = table.scan(AttributesToGet=['Date'])
+    db_data = table.scan(AttributesToGet=['Date'])
 
     date_list = []
-    for i in current_db_data["Items"]:
+    for i in db_data["Items"]:
         json_str = json.dumps(i)
         resp_dict = json.loads(json_str)
         date_list.append(resp_dict.get('Date'))
 
     count = 0
+    updated_items = []
     for i in fdata["Date"]:
         if fdata["Date"][i] not in date_list:
             table.put_item(Item= {'Date': fdata["Date"][i],
@@ -38,5 +39,19 @@ def handler(event, context):
                                 "Deaths": fdata["Deaths"][i],
                                 "Recovered": fdata["Recovered"][i]})
             count +=1
-            print ("Date {} has been loaded into the database. Cases {},  Deaths {}, Recovered {}".format(fdata["Date"][i],fdata["Cases"][i], fdata["Deaths"][i],  fdata["Recovered"][i]))
-    print ("{} dates have been added to the database.".format(count))
+            updated_items.append(("Date {} has been loaded into the database. Cases {},  Deaths {}, Recovered {}".format(fdata["Date"][i],fdata["Cases"][i], fdata["Deaths"][i],  fdata["Recovered"][i])))
+
+    message =  ("{} dates have been added to the database.".format(count) + "\n" + "\n".join(updated_items))
+    sns_arn = "arn:aws:sns:us-east-1:741539688093:dynamodb"
+    sns = boto3.client("sns")
+
+    if count > 0:
+        response = sns.publish(
+            TargetArn=sns_arn,
+            Subject=("AWS LAMBDA NOTIFICATION"),
+            Message=(message))
+    else:
+        response = sns.publish(
+            TargetArn=sns_arn,
+            Subject=("AWS LAMBDA NOTIFICATION"),
+            Message=("There were no database updates with the last ETL run."))
